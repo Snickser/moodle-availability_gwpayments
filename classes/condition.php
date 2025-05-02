e3c7b9ace366c350b2c358ea0413ebbc59c259da
Snickser <snickser@gmail.com>
2025-05-02 20:45:09 +0300

    fix

--- classes/condition.php
+++ classes/condition.php
@@ -44,9 +44,16 @@ defined('MOODLE_INTERNAL') || die();
  */
 class condition extends \core_availability\condition {
 
+    /** @var string */
     private $accountid;
+
+    /** @var string */
     private $currency;
+
+    /** @var string */
     private $cost;
+
+    /** @var string */
     private $vat;
 
     /**
